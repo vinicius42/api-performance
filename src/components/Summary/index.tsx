43f0f59cab20c0e9fef1ab530b33f982a2b2e18e
Vinicius Santos <vinicius.santos@neomode.com.br>
2022-09-20 15:39:20 -0300

    first commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,57 @@
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionContext";
+import { SummaryCard, SummaryContainer } from "./styles";
+
+export function Summary() {
+    const { transactions } = useContext(TransactionsContext);
+
+    const summary = transactions?.reduce(
+        (acc, transaction) => {
+        if (transaction.type == 'income'){
+            acc.income += transaction.price;
+            acc.total += transaction.price;
+        }
+        else {
+            acc.outcome += transaction.price;
+            acc.total -= transaction.price;
+        }
+        return acc;
+    }, 
+    {
+        income: 0, 
+        outcome: 0, 
+        total: 0
+    })
+
+    return(
+        <SummaryContainer>
+            <SummaryCard>
+                <header>
+                    <span>Entradas</span>
+                    <ArrowCircleUp size={32} color="#00b37e"/>
+                </header>
+
+                <strong>{summary.income}</strong>
+            </SummaryCard>
+
+            <SummaryCard>
+                <header>
+                    <span>Saídas</span>
+                    <ArrowCircleDown size={32} color="#f75a68"/>
+                </header>
+
+                <strong>{summary.outcome}</strong>
+            </SummaryCard>
+
+            <SummaryCard variant="green">
+                <header>
+                    <span>Entradas</span>
+                    <CurrencyDollar size={32} color="#fff"/>
+                </header>
+
+                <strong>{summary.total}</strong>
+            </SummaryCard>
+        </SummaryContainer>
+    )
+}
